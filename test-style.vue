58f20bb25d815a25536df1442a1d918cbf19744b
marlonchiu <Knight1898@outlook.com>
2025-08-19 16:57:44 +0800

    test: 测试 stylelint 修复

---
+++ test-style.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="test-component">
+    <h1>Test Component</h1>
+  </div>
+</template>
+
+<script setup>
+  // Test component
+</script>
+
+<style lang="less" scoped>
+  .test-component {
+    padding: 20px;
+    background-color: #f5f5f5;
+  }
+
+  .test-component h1 {
+    color: #333;
+    font-size: 24px;
+  }
+</style>
